bd64781cbb5b402923ddf59f506a3e86d8abe780
Chryb <chryb.q@gmail.com>
2017-11-15 10:30:06 +0100

    v1.2.0 - added tone option

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ const {
   has, 
   keys, 
   flatten, 
-  slice }          = require('lodash')
-const Random       = require('random-js')
-const shuffleArray = require('shuffle-array')
-const colors       = require('./lib/colors.js')
+  slice }           = require('lodash')
+const Random        = require('random-js')
+const shuffleArray  = require('shuffle-array')
+const defaultColors = require('./lib/colors.js')
 
 const mt = Random.engines.mt19937().autoSeed()
 
 let shuffleCache = null
+let colors = defaultColors
 
 const randomColor = (options = {}) => {
   if (!has(options, 'excluded')) options.excluded = []
   if (!has(options, 'min'))      options.min = 0
   if (!has(options, 'max'))      options.max = 9
   if (!has(options, 'shuffle'))  options.shuffle = false
+  if (!has(options, 'tone'))     options.tone = []
+
+  // if you want a specific tone than excluded is not necessary
+  if (options.tone.length > 0) {
+    options.excluded = []
+  }
 
   const allowedColors = flatten(
     keys(colors)
       .filter(k => options.excluded.indexOf(k) === -1)
+      .filter(k => {
+        return (options.tone.length > 0) 
+          ? options.tone.indexOf(k) > -1 : true
+      })
       .map(c => slice(colors[c], options.min, options.max + 1))
   )
 
@@ -32,6 +43,10 @@ const randomColor = (options = {}) => {
     const index = Random.integer(0, shuffleCache.length - 1)(mt)
     return shuffleCache.splice(index, 1)[0]
   }
+}
+
+randomColor.input = input => {
+  colors = input
 }
 
 randomColor.tone = color => {
